a15dbf7fc60f60f72d804069cd3c224638bb1243
Hardy Ferentschik <hibernate@ferentschik.de>
2014-02-24 12:58:58 +0100

    Changes needed to compile against JDK8

--- engine/src/test/java/org/hibernate/validator/test/internal/util/TypeHelperTest.java
+++ engine/src/test/java/org/hibernate/validator/test/internal/util/TypeHelperTest.java
@@ -18,6 +18,8 @@
 package org.hibernate.validator.test.internal.util;
 
 import java.io.Serializable;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedType;
 import java.lang.reflect.GenericDeclaration;
 import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
@@ -907,7 +909,7 @@ public class TypeHelperTest {
 	private static TypeVariable typeVariable(final GenericDeclaration declaration,
 											 final String name,
 											 final Type... bounds) {
-		return new TypeVariable() {
+		return new TypeVariable<GenericDeclaration>() {
 			@Override
 			public Type[] getBounds() {
 				if ( bounds == null || bounds.length == 0 ) {
@@ -924,6 +926,41 @@ public class TypeHelperTest {
 			@Override
 			public String getName() {
 				return name;
+			}
+
+			@Override
+			public AnnotatedType[] getAnnotatedBounds() {
+				return new AnnotatedType[0];
+			}
+
+			@Override
+			public <T extends Annotation> T getAnnotation(Class<T> annotationClass) {
+				return null;
+			}
+
+			@Override
+			public Annotation[] getAnnotations() {
+				return new Annotation[0];
+			}
+
+			@Override
+			public <T extends Annotation> T[] getAnnotationsByType(Class<T> annotationClass) {
+				return null;
+			}
+
+			@Override
+			public <T extends Annotation> T getDeclaredAnnotation(Class<T> annotationClass) {
+				return null;
+			}
+
+			@Override
+			public <T extends Annotation> T[] getDeclaredAnnotationsByType(Class<T> annotationClass) {
+				return null;
+			}
+
+			@Override
+			public Annotation[] getDeclaredAnnotations() {
+				return new Annotation[0];
 			}
 		};
 	}
